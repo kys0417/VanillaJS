6179c1a33a93b858d74887dac7f9fa2e8ac242f4
YS0417 <yungsang1111@gmail.com>
2021-11-20 03:00:13 +0900

    11/20 challenge answer

--- Challenge/if, else, events/app.js
+++ Challenge/if, else, events/app.js
@@ -1,11 +1,19 @@
-const h1 = document.querySelector("h1");
+const body = document.querySelector("body");
 
 function handleInnerwidth() {
-  if (window.innerWidth < 300) {
-    h1.classList.toggle("step1");
-  } else if (window.innerWidth > 300 && window.innerwidth < 600) {
-    h1.classList.toggle("step2");
-  } else h1.classList.toggle("step3");
+  if (window.innerWidth < 500) {
+    body.classList.add("step1");
+    body.classList.remove("step2");
+    body.classList.remove("step3");
+  } else if (window.innerWidth > 501 && window.innerWidth < 1000) {
+    body.classList.add("step2");
+    body.classList.remove("step1");
+    body.classList.remove("step3");
+  } else if (window.innerWidth > 1001){
+    body.classList.add("step3");
+    body.classList.remove("step1");
+    body.classList.remove("step2");
+  }
 }
 
 window.addEventListener("resize", handleInnerwidth);
